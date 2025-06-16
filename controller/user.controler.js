2df6cdcedc0a305da1b54f9d9979177acea14fbc
Valentin36 <valik.negrya@gmail.com>
2025-06-16 17:57:42 +0300

    added required inputs in createUser

--- controller/user.controler.js
+++ controller/user.controler.js
@@ -4,13 +4,17 @@ class userControler {
 
   async createUser(req, res) {
         const {login, email, password} = req.body
-        const getUsers = await db.query('SELECT * FROM users WHERE login = $1', [login])
-        if (getUsers.rows.length !== 0) res.json('user with that login already exists')
-        else {
+        if (!login || !email || !password) {
+          return res.status(400).send({message: 'Need more information'})
+        } else {
+          const getUsers = await db.query('SELECT * FROM users WHERE login = $1', [login])
+          if (getUsers.rows.length !== 0) res.json('user with that login already exists')
+          else {
             const newUser = await db.query('INSERT INTO users (login, email, password) values ($1, $2, $3) RETURNING *',
-            [login, email, password])
+              [login, email, password])
             console.log(`Account successfully created`)
             res.json(newUser.rows[0])
+          }
         }
     }
 
--- db-init.js
+++ db-init.js
@@ -3,24 +3,26 @@ const {Client} = require('pg')
 const db_name = 'node_profile'
 
 const system_client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    password: '1111',
-    port: 5432,
-    database: 'postgres'
+  user: 'postgres',
+  host: 'localhost',
+  password: '1111',
+  port: 5432,
+  database: 'postgres'
 })
 const db_client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    password: '1111',
-    port: 5432,
-    database: db_name
+  user: 'postgres',
+  host: 'localhost',
+  password: '1111',
+  port: 5432,
+  database: db_name
 })
 
 async function initDb() {
   try {
     await system_client.connect();
-    const res = await system_client.query(`SELECT 1 FROM pg_database WHERE datname = '${db_name}'`);
+    const res = await system_client.query(`SELECT 1
+                                           FROM pg_database
+                                           WHERE datname = '${db_name}'`);
     if (res.rowCount === 0) {
       await system_client.query(`CREATE DATABASE ${db_name}`);
       console.log(`BD ${db_name} CREATED`);
@@ -33,16 +35,17 @@ async function initDb() {
     await db_client.connect();
     const table = await db_client.query(`SELECT to_regclass('public.users')`);
     if (table.rows[0].to_regclass !== null) {
-        console.log('TABLE "users" already exists')
+      console.log('TABLE "users" already exists')
     } else {
-        await db_client.query(`create TABLE users(
-            id SERIAL PRIMARY KEY,
-            login VARCHAR(255),
-            email VARCHAR(255),
-            password VARCHAR(255)
-            );
-            `)
-        console.log('Table "users" CREATED')
+      await db_client.query(`create TABLE users
+                             (
+                                 id       SERIAL PRIMARY KEY,
+                                 login    VARCHAR(255),
+                                 email    VARCHAR(255),
+                                 password VARCHAR(255)
+                             );
+      `)
+      console.log('Table "users" CREATED')
     }
     await db_client.end();
   } catch (err) {
